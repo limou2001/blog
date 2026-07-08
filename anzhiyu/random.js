ca13da3f17715ac83d9cb422b5307f57dc17f1cc
李佳成 <lijiacheng@robam.com>
2026-07-08 11:47:25 +0800

    Site updated: 2026-07-08 11:47:14

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,1 +1,1 @@
-var posts=["2026/07/07/大模型基础概念/","2026/06/22/英语学习之路/"];function toRandomPost(){pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);};
+var posts=["2026/07/08/Transformer与大模型工作原理/","2026/07/07/大模型基础概念/","2026/06/22/英语学习之路/"];function toRandomPost(){pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);};
